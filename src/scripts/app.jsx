6732a47eda1d0eaf072c766655fb770da87bef63
ModuloM <modulom@modulom.net>
2015-02-15 10:19:26 +0100

    Cleaning files and structure

--- src/scripts/app.jsx
+++ src/scripts/app.jsx
@@ -3,12 +3,7 @@
 var React = require('react'),
     Module = require('./modules/Module.jsx');
 
-var data = [
-  {author: "Pete Hunt", text: "This is one comment"},
-  {author: "Jordan Walke", text: "This is *another* comment"}
-];
-
 React.render(
-    <Module data={data} />,
+    <Module />,
     document.getElementById('content')
   );
--- src/scripts/modules/Module.jsx
+++ src/scripts/modules/Module.jsx
@@ -1,13 +1,12 @@
 'use strict';
+
 var React = require('react');
-// commentaire
-// // test
 
 var Module = React.createClass({
   render: function() {
     return (
-      <div className="commentBox">
-        <h1>Comments</h1>
+      <div className="module">
+        <h1>Module</h1>
       </div>
     );
   }
